865ee5b0609440934010b96a6b1de6a96d9dc468
Johan Larsson <johan_gavle1@hotmail.com>
2015-04-30 22:58:21 +0200

    Hacked a sample.

---
+++ PathBox/CommentStripConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PathBox
+{
+    public class CommentStripConverter : IValueConverter
+    {
+        private static readonly string commentPattern = @"/\*[^\*]*\*/";
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var s = value as string;
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return null;
+            }
+            var clean = Regex.Replace(s, commentPattern, "");
+            return Geometry.Parse(clean);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
